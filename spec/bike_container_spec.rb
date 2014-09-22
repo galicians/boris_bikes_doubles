871b3b25814aac91b8b961436b48d4341cad3e73
galicians <galicians@gmail.com>
2014-09-22 19:28:26 +0100

    testing bike container using doubles

--- spec/bike_container_spec.rb
+++ spec/bike_container_spec.rb
@@ -5,31 +5,41 @@ class ContainerHolder; include BikeContainer; end
 
 describe BikeContainer do 
 
-	let(:bike) { Bike.new }
+	let(:bike) { double :bike , broken?: false }
 	let(:holder) { ContainerHolder.new }
 	let(:station) { DockingStation.new(:capacity => 123) }
 
 	it "should accept a bike" do
-		expect(holder.bike_count).to eq(0)
+		expect{holder.dock(bike)}.to change{holder.bike_count}.by 1
+	end
+
+	it "should release a bike" do
 		holder.dock(bike)
-		expect(holder.bike_count).to eq(1)
+		expect{holder.release(bike)}.to change{holder.bike_count}.by -1
 	end
 
-	it "should allow setting default capacity on initialising" do
-		expect(station.capacity).to eq(123)
-	end
 
-	it "should return the available bikes" do
-		expect(holder.bike_count).to eq(0)
-		holder.dock(bike)
-		expect(holder.bike_count).to eq(1)
-	end
+	# it "should accept a bike" do
+	# 	expect(holder.bike_count).to eq(0)
+	# 	holder.dock(bike)
+	# 	expect(holder.bike_count).to eq(1)
+	# end
 
-	it 'dock station should provide the list of broken bikes' do
-		broken_bike, working_bike = Bike.new, Bike.new
-		broken_bike.break!
-		holder.dock(broken_bike)
-		expect(holder.broken_bikes).to eq([broken_bike])
-	end
+	# it "should allow setting default capacity on initialising" do
+	# 	expect(station.capacity).to eq(123)
+	# end
+
+	# it "should return the available bikes" do
+	# 	expect(holder.bike_count).to eq(0)
+	# 	holder.dock(bike)
+	# 	expect(holder.bike_count).to eq(1)
+	# end
+
+	# it 'dock station should provide the list of broken bikes' do
+	# 	broken_bike, working_bike = Bike.new, Bike.new
+	# 	broken_bike.break!
+	# 	holder.dock(broken_bike)
+	# 	expect(holder.broken_bikes).to eq([broken_bike])
+	# end
 	
 end
